c680fe08f6c0ad08d13423e1e0d6e40e1f15e988
nmessa <nmessa@sau16.org>
2021-02-03 08:32:50 -0500

    Add files via upload

---
+++ Lab Exercise 2.3.2021/Dreamhouse/dreamhouse.py
@@ -0,0 +1,208 @@
+## Dreamhouse
+## Author: nmessa
+
+from graphics import *
+import random
+
+COLORS = ['snow', 'ghost white', 'white smoke', 'gainsboro', 'floral white', 'old lace',
+    'linen', 'antique white', 'papaya whip', 'blanched almond', 'bisque', 'peach puff',
+    'navajo white', 'lemon chiffon', 'mint cream', 'azure', 'alice blue', 'lavender',
+    'lavender blush', 'misty rose', 'dark slate gray', 'dim gray', 'slate gray',
+    'light slate gray', 'gray', 'light grey', 'midnight blue', 'navy', 'cornflower blue', 'dark slate blue',
+    'slate blue', 'medium slate blue', 'light slate blue', 'medium blue', 'royal blue',  'blue',
+    'dodger blue', 'deep sky blue', 'sky blue', 'light sky blue', 'steel blue', 'light steel blue',
+    'light blue', 'powder blue', 'pale turquoise', 'dark turquoise', 'medium turquoise', 'turquoise',
+    'cyan', 'light cyan', 'cadet blue', 'medium aquamarine', 'aquamarine', 'dark green', 'dark olive green',
+    'dark sea green', 'sea green', 'medium sea green', 'light sea green', 'pale green', 'spring green',
+    'lawn green', 'medium spring green', 'green yellow', 'lime green', 'yellow green',
+    'forest green', 'olive drab', 'dark khaki', 'khaki', 'pale goldenrod', 'light goldenrod yellow',
+    'light yellow', 'yellow', 'gold', 'light goldenrod', 'goldenrod', 'dark goldenrod', 'rosy brown',
+    'indian red', 'saddle brown', 'sandy brown',
+    'dark salmon', 'salmon', 'light salmon', 'orange', 'dark orange',
+    'coral', 'light coral', 'tomato', 'orange red', 'red', 'hot pink', 'deep pink', 'pink', 'light pink',
+    'pale violet red', 'maroon', 'medium violet red', 'violet red',
+    'medium orchid', 'dark orchid', 'dark violet', 'blue violet', 'purple', 'medium purple',
+    'thistle', 'snow2', 'snow3',
+    'snow4', 'seashell2', 'seashell3', 'seashell4', 'AntiqueWhite1', 'AntiqueWhite2',
+    'AntiqueWhite3', 'AntiqueWhite4', 'bisque2', 'bisque3', 'bisque4', 'PeachPuff2',
+    'PeachPuff3', 'PeachPuff4', 'NavajoWhite2', 'NavajoWhite3', 'NavajoWhite4',
+    'LemonChiffon2', 'LemonChiffon3', 'LemonChiffon4', 'cornsilk2', 'cornsilk3',
+    'cornsilk4', 'ivory2', 'ivory3', 'ivory4', 'honeydew2', 'honeydew3', 'honeydew4',
+    'LavenderBlush2', 'LavenderBlush3', 'LavenderBlush4', 'MistyRose2', 'MistyRose3',
+    'MistyRose4', 'azure2', 'azure3', 'azure4', 'SlateBlue1', 'SlateBlue2', 'SlateBlue3',
+    'SlateBlue4', 'RoyalBlue1', 'RoyalBlue2', 'RoyalBlue3', 'RoyalBlue4', 'blue2', 'blue4',
+    'DodgerBlue2', 'DodgerBlue3', 'DodgerBlue4', 'SteelBlue1', 'SteelBlue2',
+    'SteelBlue3', 'SteelBlue4', 'DeepSkyBlue2', 'DeepSkyBlue3', 'DeepSkyBlue4',
+    'SkyBlue1', 'SkyBlue2', 'SkyBlue3', 'SkyBlue4', 'LightSkyBlue1', 'LightSkyBlue2',
+    'LightSkyBlue3', 'LightSkyBlue4', 'SlateGray1', 'SlateGray2', 'SlateGray3',
+    'SlateGray4', 'LightSteelBlue1', 'LightSteelBlue2', 'LightSteelBlue3',
+    'LightSteelBlue4', 'LightBlue1', 'LightBlue2', 'LightBlue3', 'LightBlue4',
+    'LightCyan2', 'LightCyan3', 'LightCyan4', 'PaleTurquoise1', 'PaleTurquoise2',
+    'PaleTurquoise3', 'PaleTurquoise4', 'CadetBlue1', 'CadetBlue2', 'CadetBlue3',
+    'CadetBlue4', 'turquoise1', 'turquoise2', 'turquoise3', 'turquoise4', 'cyan2', 'cyan3',
+    'cyan4', 'DarkSlateGray1', 'DarkSlateGray2', 'DarkSlateGray3', 'DarkSlateGray4',
+    'aquamarine2', 'aquamarine4', 'DarkSeaGreen1', 'DarkSeaGreen2', 'DarkSeaGreen3',
+    'DarkSeaGreen4', 'SeaGreen1', 'SeaGreen2', 'SeaGreen3', 'PaleGreen1', 'PaleGreen2',
+    'PaleGreen3', 'PaleGreen4', 'SpringGreen2', 'SpringGreen3', 'SpringGreen4',
+    'green2', 'green3', 'green4', 'chartreuse2', 'chartreuse3', 'chartreuse4',
+    'OliveDrab1', 'OliveDrab2', 'OliveDrab4', 'DarkOliveGreen1', 'DarkOliveGreen2',
+    'DarkOliveGreen3', 'DarkOliveGreen4', 'khaki1', 'khaki2', 'khaki3', 'khaki4',
+    'LightGoldenrod1', 'LightGoldenrod2', 'LightGoldenrod3', 'LightGoldenrod4',
+    'LightYellow2', 'LightYellow3', 'LightYellow4', 'yellow2', 'yellow3', 'yellow4',
+    'gold2', 'gold3', 'gold4', 'goldenrod1', 'goldenrod2', 'goldenrod3', 'goldenrod4',
+    'DarkGoldenrod1', 'DarkGoldenrod2', 'DarkGoldenrod3', 'DarkGoldenrod4',
+    'RosyBrown1', 'RosyBrown2', 'RosyBrown3', 'RosyBrown4', 'IndianRed1', 'IndianRed2',
+    'IndianRed3', 'IndianRed4', 'sienna1', 'sienna2', 'sienna3', 'sienna4', 'burlywood1',
+    'burlywood2', 'burlywood3', 'burlywood4', 'wheat1', 'wheat2', 'wheat3', 'wheat4', 'tan1',
+    'tan2', 'tan4', 'chocolate1', 'chocolate2', 'chocolate3', 'firebrick1', 'firebrick2',
+    'firebrick3', 'firebrick4', 'brown1', 'brown2', 'brown3', 'brown4', 'salmon1', 'salmon2',
+    'salmon3', 'salmon4', 'LightSalmon2', 'LightSalmon3', 'LightSalmon4', 'orange2',
+    'orange3', 'orange4', 'DarkOrange1', 'DarkOrange2', 'DarkOrange3', 'DarkOrange4',
+    'coral1', 'coral2', 'coral3', 'coral4', 'tomato2', 'tomato3', 'tomato4', 'OrangeRed2',
+    'OrangeRed3', 'OrangeRed4', 'red2', 'red3', 'red4', 'DeepPink2', 'DeepPink3', 'DeepPink4',
+    'HotPink1', 'HotPink2', 'HotPink3', 'HotPink4', 'pink1', 'pink2', 'pink3', 'pink4',
+    'LightPink1', 'LightPink2', 'LightPink3', 'LightPink4', 'PaleVioletRed1',
+    'PaleVioletRed2', 'PaleVioletRed3', 'PaleVioletRed4', 'maroon1', 'maroon2',
+    'maroon3', 'maroon4', 'VioletRed1', 'VioletRed2', 'VioletRed3', 'VioletRed4',
+    'magenta2', 'magenta3', 'magenta4', 'orchid1', 'orchid2', 'orchid3', 'orchid4', 'plum1',
+    'plum2', 'plum3', 'plum4', 'MediumOrchid1', 'MediumOrchid2', 'MediumOrchid3',
+    'MediumOrchid4', 'DarkOrchid1', 'DarkOrchid2', 'DarkOrchid3', 'DarkOrchid4',
+    'purple1', 'purple2', 'purple3', 'purple4', 'MediumPurple1', 'MediumPurple2',
+    'MediumPurple3', 'MediumPurple4', 'thistle1', 'thistle2', 'thistle3', 'thistle4',
+    'gray1', 'gray2', 'gray3', 'gray4', 'gray5', 'gray6', 'gray7', 'gray8', 'gray9', 'gray10',
+    'gray11', 'gray12', 'gray13', 'gray14', 'gray15', 'gray16', 'gray17', 'gray18', 'gray19',
+    'gray20', 'gray21', 'gray22', 'gray23', 'gray24', 'gray25', 'gray26', 'gray27', 'gray28',
+    'gray29', 'gray30', 'gray31', 'gray32', 'gray33', 'gray34', 'gray35', 'gray36', 'gray37',
+    'gray38', 'gray39', 'gray40', 'gray42', 'gray43', 'gray44', 'gray45', 'gray46', 'gray47',
+    'gray48', 'gray49', 'gray50', 'gray51', 'gray52', 'gray53', 'gray54', 'gray55', 'gray56',
+    'gray57', 'gray58', 'gray59', 'gray60', 'gray61', 'gray62', 'gray63', 'gray64', 'gray65',
+    'gray66', 'gray67', 'gray68', 'gray69', 'gray70', 'gray71', 'gray72', 'gray73', 'gray74',
+    'gray75', 'gray76', 'gray77', 'gray78', 'gray79', 'gray80', 'gray81', 'gray82', 'gray83',
+    'gray84', 'gray85', 'gray86', 'gray87', 'gray88', 'gray89', 'gray90', 'gray91', 'gray92',
+    'gray93', 'gray94', 'gray95', 'gray97', 'gray98', 'gray99']
+
+win = GraphWin("Dreamhouse", 1024, 768)
+win.setCoords(0.0, 0.0, 10.0, 10.0)
+win.setBackground('SkyBlue1')
+ground = Rectangle(Point(0,5), Point(10,0))
+ground.setFill('saddle brown')
+ground.draw(win)
+
+message = Text(Point(5, 0.5), " ")
+message.draw(win)
+message.setText("Click two points to draw your house body")
+
+# Draw the house
+p1 = win.getMouse()
+p2 = win.getMouse()
+color = random.choice(COLORS)
+house = Rectangle(p1, p2)
+house.setFill(color)
+house.draw(win)
+windowHeight = (p1.y - p2.y)/5
+doorHeight = (p1.y - p2.y)/3
+windowWidth = windowHeight/3
+doorWidth = doorHeight/3
+
+# Draw the roof
+message.setText("Where do you want the top of the roof to be?")
+p3 = p1
+p4 = Point(p2.x, p1.y)
+p5 = win.getMouse()
+roofColor = random.choice(COLORS)
+roof = Polygon(p3, p4, p5)
+roof.setFill(roofColor)
+roof.draw(win)
+
+#Draw the chimney
+message.setText("Where do you want the top or the chimney?")
+p6 = win.getMouse()
+p7 = Point(p6.x + 0.5, p1.y)
+color = random.choice(COLORS)
+chimney = Rectangle(p6, p7)
+chimney.setFill(color)
+chimney.draw(win)
+roof = Polygon(p3, p4, p5)
+roof.setFill(roofColor)
+roof.draw(win)
+
+# Draw the door
+message.setText("Where do you want the door?")
+p = win.getMouse()
+p8 = Point(p.x - doorWidth/2, p2.y + doorHeight)
+p9 = Point(p.x + doorWidth/2, p2.y)
+color = random.choice(COLORS)
+door = Rectangle(p8, p9)
+door.setFill(color)
+door.draw(win)
+
+# Draw round window
+message.setText("Where do you want the round window?")
+p = win.getMouse()
+color = random.choice(COLORS)
+roundWindow = Circle(p, windowHeight/2)
+roundWindow.setFill(color)
+roundWindow.draw(win)
+
+# Draw rectangular window
+message.setText("Where do you want the rectangular window?")
+p = win.getMouse()
+color = random.choice(COLORS)
+p10 = Point(p.x - windowWidth, p.y + windowHeight/2)
+p11 = Point(p.x + windowWidth, p.y - windowHeight/2)
+squareWindow = Rectangle(p10, p11)
+squareWindow.setFill(color)
+squareWindow.draw(win)
+
+# Draw two square windows
+for i in range(2):
+    message.setText("Where do you want the square window?")
+    p = win.getMouse()
+    color = random.choice(COLORS)
+    p10 = Point(p.x - windowHeight/2, p.y + windowHeight/2)
+    p11 = Point(p.x + windowHeight/2, p.y - windowHeight/2)
+    squareWindow = Rectangle(p10, p11)
+    squareWindow.setFill(color)
+    squareWindow.draw(win)
+
+# Draw the Sun
+message.setText("Where do you want the Sun?")
+p = win.getMouse()
+x = [0, 1.4, 2, 1.4, 0, -1.4, -2, -1.4] 
+y = [2, 1.4, 0, -1.4, -2, -1.4, 0, 1.4]
+sun = Circle(p, 1)
+sun.setOutline('yellow')
+sun.setFill('yellow')
+sun.draw(win)
+for i in range(len(x)):
+    line = Line(p, Point(p.x+x[i],p.y+y[i]))
+    line.setFill('yellow')
+    line.draw(win)
+
+#place a lake
+message.setText("Click two points to define your lake?")
+p1 = win.getMouse()
+p2 = win.getMouse()
+lake = Oval(p1, p2)
+lake.setFill('blue')
+lake.draw(win)
+
+#plant 10 trees
+message.setText("Where do you want to plant a tree?")
+for i in range(10):
+    p = win.getMouse()
+    trunk = Line(p, Point(p.x, p.y + 0.6))
+    trunk.setWidth(10)
+    trunk.setFill('red')
+    trunk.draw(win)
+    pa = Point(p.x - 0.3, p.y + 0.6)
+    pb = Point(p.x, p.y + 1.5)
+    pc = Point(p.x + 0.3, p.y + 0.6)
+    tree = Polygon(pa, pb, pc)
+    tree.setFill('green')
+    tree.draw(win)
+
+message.setText("Click anywhere to quit.")
+win.getMouse()
+win.close()
+
