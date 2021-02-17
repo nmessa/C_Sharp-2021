17edb73d5dfe50aa4abcc335e69c6becaa1b46aa
nmessa <nmessa@sau16.org>
2021-02-17 07:13:59 -0500

    Add files via upload

--- Lab Exercise 2.17.2021/RangeMatch Demo/RangeMatch Demo/Program.cs
+++ Lab Exercise 2.17.2021/RangeMatch Demo/RangeMatch Demo/Program.cs
@@ -9,16 +9,27 @@ namespace RangeMatch_Demo
     {
         static void Main(string[] args)
         {
-            int numOfItems, discount;
+            //declare variables
+            int numOfItems, discount, index;
+            double customerDiscount;
+
+            //Get number of items to purchase from user
             Console.Write("Enter the number of items to purchase: ");
             numOfItems = Convert.ToInt32(Console.ReadLine());
+
+            //Create arrays with range low limits and discounts
             int[] discountRangeLowLimit = { 1, 13, 50, 100, 200 };
             double[] discounts = { 0, 0.10, 0.14, 0.18, 0.20 };
-            double customerDiscount;
-            int index = discountRangeLowLimit.Length - 1;
+            
+            //Find the index to determine discount
+            index = discountRangeLowLimit.Length - 1;
             while (index >= 0 && numOfItems < discountRangeLowLimit[index])
                 index--;
+
+            //Get discount
             customerDiscount = discounts[index];
+
+            //Convert discount to percent
             discount = Convert.ToInt32(customerDiscount * 100);
             Console.WriteLine("Your discount is {0}%", discount);
         }
