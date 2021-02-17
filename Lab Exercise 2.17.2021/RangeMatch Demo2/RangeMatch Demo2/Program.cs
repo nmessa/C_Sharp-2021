6f02ef877776dfd57ef0d664dbdd8cbba976c6fd
nmessa <nmessa@sau16.org>
2021-02-17 07:15:14 -0500

    Add files via upload

--- Lab Exercise 2.17.2021/RangeMatch Demo2/RangeMatch Demo2/Program.cs
+++ Lab Exercise 2.17.2021/RangeMatch Demo2/RangeMatch Demo2/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Range match demo using upper limits for ranges
+//Author: nmessa
+//Date: 2.17.2021
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,17 +13,31 @@ namespace RangeMatch_Demo2
     {
         static void Main(string[] args)
         {
+            //declare variables
             int numOfItems, discount;
+            double customerDiscount;
+            int index = 0;
+
+            //Get number of items to purchase from user
             Console.Write("Enter the number of items to purchase: ");
             numOfItems = Convert.ToInt32(Console.ReadLine());
+
+            //Declare arrays of discounts and range limits
             int[] discountRangeUpperLimit = { 12, 49, 99, 199, 9999999 };
             double[] discounts = { 0, 0.10, 0.14, 0.18, 0.20 };
-            double customerDiscount;
-            int sub = 0;
-            while (sub < discountRangeUpperLimit.Length && numOfItems > discountRangeUpperLimit[sub])
-                ++sub;
-            customerDiscount = discounts[sub];
+            
+            //Finde the index for discount
+            while (index < discountRangeUpperLimit.Length && 
+                numOfItems > discountRangeUpperLimit[index])
+                ++index;
+
+            //Get discount
+            customerDiscount = discounts[index];
+
+            //Convert discount to percent
             discount = Convert.ToInt32(customerDiscount * 100);
+
+            //Output result
             Console.WriteLine("Your discount is {0}%", discount);
         }
     }
