29723fb357162de0753bc7b5e223e8961422a17d
nmessa <nmessa@sau16.org>
2021-02-21 11:20:50 -0500

    Add files via upload

---
+++ Lab Exercise 3.4.2021/Currency Converter/Currency Converter/Form1.cs
@@ -0,0 +1,47 @@
+﻿//Lab Exercise 3.4.2021 Problem 2
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Currency_Converter
+{
+    public partial class Form1 : Form
+    {
+        const double dblYEN_FACTOR = 110.81;
+        const double dblEURO_FACTOR = 0.88;
+        const double dblPOUND_FACTOR = 0.76;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double dollars, pounds, euros, yen;
+            //Add code here
+
+
+        }
+    }
+}
---
+++ Lab Exercise 3.4.2021/Widget/Widget/Form1.cs
@@ -0,0 +1,43 @@
+﻿//Lab Exercise 3.4.2021 Problem 1
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Widget
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int gross, tare, widgets;
+            //Add code here
+
+
+        }
+    }
+}
