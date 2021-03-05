e71f31e71acdee3b681dc751b924237958b4b7ae
nmessa <nmessa@sau16.org>
2021-03-05 14:14:34 -0500

    Add files via upload

---
+++ Lab Exercise 3.8.2021/Pizza Palace/Pizza Palace/Form1.cs
@@ -0,0 +1,44 @@
+﻿//Lab Exercise 3.8.2021 Problem 2
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Pizza_Palace
+{
+    public partial class Form1 : Form
+    {
+        const double dblSLICE = 14.125;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double diameter, area;
+            int slices;
+            //Add code here
+
+        }
+    }
+}
---
+++ Lab Exercise 3.8.2021/PropertyTax/PropertyTax/Form1.cs
@@ -0,0 +1,44 @@
+﻿//Lab Exercise 3.8.2021 Problem 1
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PropertyTax
+{
+    public partial class Form1 : Form
+    {
+        const double dblMIL_RATE = 6.4;
+        const double dblFACTOR = 0.6;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double market, value, tax;
+            //Add code here
+
+        }
+    }
+}
