2de30bfd2a444ed3a575498c3aa4549ecf647643
nmessa <nmessa@sau16.org>
2021-03-26 14:47:04 -0400

    Add files via upload

---
+++ Lab Exercise 3.29.2021/Jackson Pollack/Jackson Pollack/Form1.cs
@@ -0,0 +1,71 @@
+﻿//Lab Exercise 3.29.2021
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Jackson_Pollack
+{
+    public partial class Form1 : Form
+    {
+        //Create global random number generator object
+        Random r = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            //Hide form controls
+            //Add code here
+
+            //Declare local variables
+            int intTimes, rShape, rX, rY, rWidth, rHeight, rColor, index;
+            bool printPainting = false;
+
+            //Create a white graphics surface to draw on
+            //Add code here
+
+
+            //Get the number of shapes to draw
+            //Add code here
+
+
+            //Create drawing objects
+            //Add code here
+
+
+            //Create the shapes
+            //Add code here
+
+            
+
+            //Check if print desired
+            //Add code here
+
+
+
+            //Send image to default printer
+            //Add code here
+        
+
+
+            //Pause program for 5 seconds
+            //Add code here
+
+
+            //Restore the form to it's starting state
+            //Add code here
+
+        }
+    }
+}
---
+++ Lab Exercise 3.31.2021/Sin Plotter/Sin Plotter/Form1.cs
@@ -0,0 +1,39 @@
+﻿//Lab Exercise 3.31.2021 Problem 1
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sin_Plotter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlot_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
