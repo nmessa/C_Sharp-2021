2ab8dac189477a7a24e3103aba5f17bafc659082
nmessa <nmessa@sau16.org>
2021-04-16 15:03:11 -0400

    Add files via upload

---
+++ Lab Exercise 4.21.2021/Half Life/Half Life/Form1.cs
@@ -0,0 +1,55 @@
+﻿//Lab Exercise 4.21.2021 Problem 2
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Half_Life
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAge_Click(object sender, EventArgs e)
+        {
+            //Declare variables
+            double age, halfLife, initialCount, finalCount;
+
+            //Get values from textboxes
+            //Add code here
+
+            //Check for invalid data
+            if (initialCount < finalCount)
+            {
+                MessageBox.Show("Invalid data");
+                //Reset textboxes and set focus to txtHalfLife
+                txtFinalCount.Text = "";
+                txtHalfLife.Text = "";
+                txtInitialCount.Text = "";
+                txtHalfLife.Focus();
+                return;
+            }
+
+            //Calculate age
+            //Add code here
+
+
+            //Store result in label
+            //Add code here
+
+
+            //Reset textboxes and set focus to txtHalfLife
+            //Add code here
+
+        }
+    }
+}
---
+++ Lab Exercise 4.21.2021/Ski Wax/Ski Wax/Form1.cs
@@ -0,0 +1,72 @@
+﻿//Lab Exercise 4.21.2021 Problem 1
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ski_Wax
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWax_Click(object sender, EventArgs e)
+        {
+            //Declare variables
+            int temperature;
+            string conditions;
+            string message;
+            string variety = null;
+            string group = null;
+
+            //Get temperature from textbox and store as integer
+            //Add code here
+
+
+            //Check for valid temperature
+            //if not valid, give message and reset
+            if (temperature < -50 || temperature > 100)
+            {
+                //Add code here
+
+
+                return;
+            }
+
+            //Get selected item from combobox and store in conditions
+            //Add code here
+
+
+            //Determine variety
+            switch (conditions)
+            {
+                //Add code here
+
+            }
+
+            //Determine group
+            //Add code here
+
+
+            //Output the result
+            message = "The best wax is: " + variety + " " + group;
+            lblType.Text = message;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+
+        }
+    }
+}
