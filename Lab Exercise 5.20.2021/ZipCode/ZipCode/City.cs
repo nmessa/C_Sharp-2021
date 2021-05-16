914d3f7462229e22d9f8f9ef0a9f2cf107d95080
nmessa <nmessa@sau16.org>
2021-05-16 09:11:00 -0400

    Add files via upload

---
+++ Lab Exercise 5.20.2021/ZipCode/ZipCode/City.cs
@@ -0,0 +1,25 @@
+﻿//Lab Exercise 5.20.2021
+//City Class
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZipCode
+{
+    class City
+    {
+        private string rid;
+        private string zipCode;
+        private string city;
+        private string state;
+
+        //Class Constructor
+        public City(string id, string zc, string cty, string st)
+        {
+            //Add code here
+
+        }
+    }
+}
---
+++ Lab Exercise 5.20.2021/ZipCode2/ZipCode2/City.cs
@@ -0,0 +1,44 @@
+﻿//Lab Exercise 5.20.2021
+//City Class
+//Author: nmessa
+//Added getState, getCity, and getZIP class methods
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZipCode2
+{
+    class City
+    {
+        private string rid;
+        private string zipCode;
+        private string city;
+        private string state;
+
+        public City(string id, string zc, string cty, string st)
+        {
+            //Add code here
+
+        }
+
+        public string getState()
+        {
+            //Add code here
+
+        }
+
+        public string getCity()
+        {
+            //Add code here
+
+        }
+
+        public string getZIP()
+        {
+            //Add code here
+
+        }
+    }
+}
