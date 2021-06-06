dbee50c528529f0f69059c6ed353028095a1c82b
nmessa <nmessa@sau16.org>
2021-06-06 12:49:27 -0400

    Add files via upload

---
+++ Lab Exercise 6.7.2021/Skate/Skate/Form1.cs
@@ -0,0 +1,49 @@
+﻿//Lab Exercise 6.7.2021
+//Skate!!!
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Skate
+{
+    public partial class Form1 : Form
+    {
+        //Define the number of images
+        //Add code here
+
+
+        //Create an array of image file names
+        //Add code here
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrChangeImage_Tick(object sender, EventArgs e)
+        {
+            //display an image from array
+            //Add code here
+
+
+            //determine next image number
+            //Add code here
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //store image file names in array
+            //Add code here
+
+        }
+    }
+}
