5d4efeb221d5150700dd5ab183aa14136ec22051
nmessa <nmessa@sau16.org>
2021-06-12 13:03:04 -0400

    Add files via upload

---
+++ Lunar Lander/Lunar Lander/Form1.cs
@@ -0,0 +1,82 @@
+﻿//Lunar Lander Game
+//Author: 
+//Date: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lunar_Lander
+{
+    public partial class Form1 : Form
+    {
+        //Declare global variables
+        //Add code here
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        //This event creates a new game
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        //This event either stops the game or exits the program
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        //This event executes an engine burn using the allocated amount of fuel
+        private void btnBurn_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+
+
+        }
+
+
+        //This event updates rocket ship parameters
+        private void tmrFalling_Tick(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        //This method evaluates your landing
+        private void Landed()
+        {
+            //Add code here
+
+            
+            
+    
+            
+        }
+
+        //This method updates the rocket position
+        private void DrawRocket()
+        {
+            //Add code here
+
+        }
+    }
+}
